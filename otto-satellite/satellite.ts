92de2cbf428d99cd0c7e1fc6450f8fdee0486472
Zack Hicks <zhicks@osisoft.com>
2018-04-26 13:11:47 -0400

    asdfasdfds

--- otto-satellite/satellite.ts
+++ otto-satellite/satellite.ts
@@ -173,8 +173,16 @@ module OttoSatelliteModule {
     const BashScript = `
         git pull;
         pkill -f node;
-        npm run start;
     `;
+
+    process.on("exit", function () {
+        require("child_process").spawn(process.argv.shift(), process.argv, {
+            cwd: process.cwd(),
+            detached : true,
+            stdio: "inherit"
+        });
+    });
+    process.exit();
 
     new OttoSatellite().init();
 }
