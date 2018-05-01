b03b083014c5ce064d7974d1167c451ce7de3ef6
Zack Hicks <zhicks@osisoft.com>
2018-05-01 19:42:05 -0400

    dasdfadfsadsf

--- otto-satellite/satellite.ts
+++ otto-satellite/satellite.ts
@@ -198,7 +198,9 @@ module OttoSatelliteModule {
     const BashScript = () => { return `
         pkill -f node;
         cd /home/${USERNAME}/otto/otto-satellite;
+        npm install;
         git stash;
+        git clean  -d  -fx .;
         git pull;
         npm run start-prod;
     `};
